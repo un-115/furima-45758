c475a01a61b74528a1fba7edc452fbf3a0e99f75
un-115 <techcamp.yuna115@gmail.com>
2025-07-18 16:33:53 +0900

    新規登録機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :items, dependent: :destroy
+  has_many :orders, dependent: :destroy
+
+  validates :nickname:, presence: true
+  validates :birthday:, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name
+    validates :last_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: '全角カタカナを使用してください' } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
 end
