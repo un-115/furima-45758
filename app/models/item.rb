7299a03d22e59f2ad367b5406ee43141a8834a3e
un-115 <techcamp.yuna115@gmail.com>
2025-07-19 18:37:14 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :fee_payer
+  belongs_to :prefecture
+  belongs_to :shopping_day
+
   belongs_to :user
   has_one_attached :image
 end
