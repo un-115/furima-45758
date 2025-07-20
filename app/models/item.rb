fa849dc7c95c565ae621bd902b44c96f4a0734eb
un-115 <techcamp.yuna115@gmail.com>
2025-07-20 20:34:35 +0900

    バリデーションにエラーメッセージを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,7 +15,7 @@ class Item < ApplicationRecord
   validates :price, presence: true,
                     numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
-  with_options presence: true, numericality: { other_than: 1 } do
+  with_options presence: true, numericality: { other_than: 1, message: "can't be blank" } do
     validates :category_id
     validates :status_id
     validates :fee_payer_id
