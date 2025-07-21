92d0cb711d2fcbff1ac680c972e5d56d16b0192b
un-115 <techcamp.yuna115@gmail.com>
2025-07-21 15:27:30 +0900

    単体テストの追加（ユーザーの紐付け）

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -72,6 +72,11 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is not a number')
       end
+      it 'ユーザーが紐付いていないと出品できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
+      end
     end
   end
 end
