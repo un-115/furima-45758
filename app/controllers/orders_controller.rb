b09e18bb190acf02760a3ac182a249fddb5d5985
un-115 <techcamp.yuna115@gmail.com>
2025-07-23 23:28:59 +0900

    売却済み商品の画面推移の条件訂正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,7 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_item
+  before_action :redirect_if_invalid_access
   def index
     gon.public_key = ENV['PAYJP_PUBLIC_KEY']
     @order_address = OrderAddress.new
@@ -23,6 +25,12 @@ class OrdersController < ApplicationController
   def set_item
     @item = Item.find(params[:item_id])
     @item_fee_payer = @item.fee_payer if @item.respond_to?(:fee_payer)
+  end
+
+  def redirect_if_invalid_access
+    return unless @item.user == current_user || @item.order.present?
+
+    redirect_to root_path
   end
 
   def order_params
--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -53,7 +53,7 @@ RSpec.describe OrderAddress, type: :model do
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include('Phone number is too short')
       end
-      it '電話番号にハイフンが含まれていると購入できない' do
+      it '電話番号に半角数値以外が含まれていると購入できない' do
         @order_address.phone_number = '090-1234-5678'
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include('Phone number is invalid. Input only number')
